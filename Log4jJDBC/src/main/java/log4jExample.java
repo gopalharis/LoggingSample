40ef7f69f211df6aff8c3b8e6dcaf231d85c3dd7
Gopal <gopalharis@gmail.com>
2016-06-23 16:35:11 +0530

    Update log4jExample.java

--- Log4jJDBC/src/main/java/log4jExample.java
+++ Log4jJDBC/src/main/java/log4jExample.java
@@ -2,7 +2,7 @@ import org.apache.log4j.Logger;
 
 
 public class log4jExample {
-static final Logger logger=Logger.getLogger(log4jExample.class.getName());
+static final Logger logger=Logger.getLogger(log4jExample.getClass());
 public static void main(String[] args) {
 	logger.debug("debug");
 	logger.info("info");
